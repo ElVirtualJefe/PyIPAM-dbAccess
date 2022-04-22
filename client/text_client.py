f8d3a361c86d992508c88d96119eadcc60641bd0
Jeff Phelps <phelps.jeffp@gmail.com>
2022-04-21 22:35:34 -0500

    update - 2022-04-21

--- client/text_client.py
+++ client/text_client.py
@@ -2,12 +2,12 @@ from stubs import ipAddress_pb2,ipAddress_pb2_grpc
 from grpc_service_connector import GrpcServiceConnector
 
 class TextClient(object):
-    #
+    
     def __init__(self):
         self.ipAddress_conn = GrpcServiceConnector(ipAddress_pb2_grpc.IpAddressServiceStub)
-        #
+        
         self.ipAddress_conn.start()
-    #
+    
     def get_ip_address_by_id(self,id):
         req = ipAddress_pb2.IpAddressIdRequest(id=id)
         res = self.ipAddress_conn.stub.GetIpAddressById(req)
